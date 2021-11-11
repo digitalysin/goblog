be7d3b8fd78a8063f10095bb648432375091125c
ɐʇɹɐʎpıʍ ɥıןɐƃ <digitalysin@yahoo.co.id>
2021-11-11 16:36:32 +0700

    add ftp list

--- ftp/ftp.go
+++ ftp/ftp.go
@@ -8,10 +8,21 @@ import (
 )
 
 type (
+	EntryType string
+
 	Ftp interface {
 		Get(path string, dst io.Writer) error
 		Put(path string, src io.Reader) error
+		List(path string) ([]Entry, error)
 		Close() error
+	}
+
+	Entry struct {
+		Name   string
+		Target string
+		Type   EntryType
+		Size   uint64
+		Time   time.Time
 	}
 
 	auth struct {
@@ -22,6 +33,12 @@ type (
 		ftp  *gftp.ServerConn
 		auth *auth
 	}
+)
+
+const (
+	Folder  = EntryType("FOLDER")
+	File    = EntryType("FILE")
+	SymLink = EntryType("SYMBOLIC_LINK")
 )
 
 func (a *auth) IsEmptyAuth() bool {
@@ -53,6 +70,44 @@ func (i *impl) Get(path string, dst io.Writer) error {
 
 func (i *impl) Put(path string, src io.Reader) error {
 	return i.ftp.Stor(path, src)
+}
+
+func (i *impl) List(path string) ([]Entry, error) {
+	var (
+		entries, err = i.ftp.List(path)
+		result       = make([]Entry, 0)
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		var (
+			res = Entry{
+				Name:   entry.Name,
+				Size:   entry.Size,
+				Time:   entry.Time,
+				Target: entry.Target,
+			}
+		)
+
+		if entry.Type == gftp.EntryTypeFile {
+			res.Type = File
+		}
+
+		if entry.Type == gftp.EntryTypeFolder {
+			res.Type = Folder
+		}
+
+		if entry.Type == gftp.EntryTypeLink {
+			res.Type = SymLink
+		}
+
+		result = append(result, res)
+	}
+
+	return result, nil
 }
 
 func New(address, user, password string, timeout time.Duration) (Ftp, error) {
